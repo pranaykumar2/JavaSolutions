20db767d1e47bf27e779088919df54a1446ea6ab
Pranay Kumar <151812978+pranaykumar2@users.noreply.github.com>
2024-10-24 06:51:35 +0530

    Update GCDCalculator.java

--- com/TCSNQT/GCDCalculator.java
+++ com/TCSNQT/GCDCalculator.java
@@ -1,8 +1,7 @@
 package com.TCSNQT;
-
 import java.util.Scanner;
 
-public class GCDCalculator {
+public class GCDLCMCalculator {
     public static int findGCD(int num1, int num2) {
         while (num2 != 0) {
             int temp = num2;
@@ -11,14 +10,27 @@ public class GCDCalculator {
         }
         return num1;
     }
+    
+    public static int findLCM(int num1, int num2) {
+        int gcd = findGCD(num1, num2);
+        return Math.abs((num1 * num2) / gcd);  ll
+    }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
         System.out.print("Enter first number: ");
         int num1 = input.nextInt();
+        
         System.out.print("Enter second number: ");
         int num2 = input.nextInt();
+        
         int gcd = findGCD(num1, num2);
+        int lcm = findLCM(num1, num2);
+        
         System.out.println("GCD: " + gcd);
+        System.out.println("LCM: " + lcm);
+        
+        input.close();
     }
 }
